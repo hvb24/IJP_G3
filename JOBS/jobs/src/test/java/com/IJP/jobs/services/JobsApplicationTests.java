ae4f209d58ff663f6cd009f0e30f95a55f4d696e
Harsh Baderiya <hvbaderiya24@gmail.com>
2024-08-08 12:26:14 +0530

    Initial Commit

---
+++ JOBS/jobs/src/test/java/com/IJP/jobs/services/JobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IJP.jobs.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
