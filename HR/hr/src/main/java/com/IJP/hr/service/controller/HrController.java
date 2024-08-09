80301bfc064c05deceefd2fc8c6c9cd0e8a98608
vishal.goswami <vishalgoswami02018@gmail.com>
2024-08-09 14:47:18 +0530

    added the Hr exception class, Global exception class, Response entity exception class, the error payload class.

--- HR/hr/src/main/java/com/IJP/hr/service/controller/HrController.java
+++ HR/hr/src/main/java/com/IJP/hr/service/controller/HrController.java
@@ -3,6 +3,8 @@ package com.IJP.hr.service.controller;
 import com.IJP.hr.service.entity.HrEntity;
 import com.IJP.hr.service.services.HrService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +15,21 @@ public class HrController {
     private HrService hrService;
 
     @GetMapping("/{email}")
-    public HrEntity getHrByEmail(@PathVariable String email) {
-        return hrService.getHrByEmail(email);
+    public ResponseEntity<HrEntity> getHrByEmail(@PathVariable String email) {
+        HrEntity res = hrService.getHrByEmail(email);
+        if(res == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @PostMapping
-    public HrEntity postHr(@RequestBody HrEntity hrEntity) {
-        return hrService.postHr(hrEntity);
+    public ResponseEntity<HrEntity> postHr(@RequestBody HrEntity hrEntity) {
+        HrEntity res = hrService.postHr(hrEntity);
+        if(res == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
     }
 
 }
